c9e5de4b1a658efb6be5ed8aff4374aad9c173be
Ivan Martynyuk <ivnmrtk@gmail.com>
2019-03-28 00:50:14 +0700

    Fix tests

--- src/test/java/my/demo/usersmanagement/UserRepositoryTest.java
+++ src/test/java/my/demo/usersmanagement/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package my.demo.usersmanagement;
 import my.demo.usersmanagement.domain.User;
 import my.demo.usersmanagement.repository.UserRepository;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import java.util.Optional;
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
+@Transactional
 @SpringBootTest
 public class UserRepositoryTest {
 
@@ -28,8 +30,20 @@ public class UserRepositoryTest {
 
     @Test
     public void findByLoginAndPassword(){
-        Optional <User> user =  userRepository.findByLoginAndPassword("larisa", "qwerty");
-        assertTrue(user.isPresent());
-        assertEquals(user.get().getLogin(), "larisa");
+        Optional <User> persistedUser =  userRepository.findByLoginAndPassword("larisa", "qwerty");
+        assertTrue(persistedUser.isPresent());
+        assertEquals(persistedUser.get().getLogin(), "larisa");
+        assertEquals(persistedUser.get().getPassword(), "qwerty");
+        assertEquals(persistedUser.get().getBlocked(), false);
     }
+
+    @Test
+    @Ignore
+    public void addUserTest(){
+        User newUser = new User();
+        newUser.setLogin("test1");
+        newUser.setPassword("test1");
+        userRepository.save(newUser);
+    }
+
 }
